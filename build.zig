7828411d66b446ab3f515eb6db0a8f560a3d229f
Maxim Kozlov <maximko.ca@gmail.com>
2026-06-09 15:30:50 -0400

    use zig build system

---
+++ build.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{
+        .preferred_optimize_mode = .Debug
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "math_interp",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .optimize = optimize,
+            .target = target
+        })
+    });
+
+    const tests = b.addTest(.{
+        .root_module = b.createModule(
+            .{
+                .root_source_file = b.path("src/main.zig"),
+                .target = target
+            }
+        )
+    });
+
+    b.installArtifact(exe);
+    b.installArtifact(tests);
+
+    const run_exe = b.addRunArtifact(exe);
+    const run_tests = b.addRunArtifact(tests);
+
+
+    const run_step = b.step("run", "Run the application");
+    const test_step = b.step("test", "Run the tests.");
+
+    run_step.dependOn(&run_exe.step);
+    test_step.dependOn(&run_tests.step);
+
+
+}
