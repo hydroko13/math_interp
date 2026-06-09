9ee33931f9b85cc66ece4ea30ea2f47571b937e4
Maxim Kozlov <maximko.ca@gmail.com>
2026-06-09 16:04:46 -0400

    Check for memory leaks when deinitializing DebugAllocator

--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,21 @@ const std = @import("std");
 const core = @import("core.zig");
 const lexer = @import("lexer.zig");
 
+
 pub fn main() !void {
     const text = "3 + 2 / 8";
 
-    var gpa = std.heap.DebugAllocator(.{}){};
+    var gpa = std.heap.DebugAllocator(.{}).init;
+
+    defer {
+        const memResult = gpa.deinit();
+        switch (memResult) {
+            .leak => {
+                std.debug.print("Memory leak detected!\n", .{});
+            },
+            .ok => {}
+        }
+    }
 
     const allocator = gpa.allocator();
 
